a749e36ccafe8eba5dac9008f9f81459b7c5c755
ashalyconceicao <145625001+ashalyconceicao@users.noreply.github.com>
2024-02-07 00:10:41 +0000

    Add files via upload

---
+++ healthy.py
@@ -0,0 +1,15 @@
+print("Healthy Foods List:")
+healthy_foods = [
+    "Vegetables",
+    "Fruits",
+    "Whole grains",
+    "Lean proteins (chicken, fish, tofu)",
+    "Nuts and seeds",
+    "Legumes (beans, lentils)",
+    "Low-fat dairy or dairy alternatives",
+    "Healthy fats (olive oil, avocados)",
+    "Water"
+]
+
+for food in healthy_foods:
+    print(f"- {food}")
